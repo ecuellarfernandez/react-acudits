cb6c2f2ad5c4b8fa665e1cd9918ebd32d600e389
ecuellarfernandez <ecuellarfernandez@gmail.com>
2021-03-22 09:33:55 +0100

    Nivel 1 ej 1 completado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+
+  const url = 'https://icanhazdadjoke.com/';
+  const [joke, setJoke] = useState('')
+
+  const getData = async ()=> {
+    try{
+      const res = await fetch(url, {headers:{'Accept': 'application/json'}});
+      const data = await res.json();
+      setJoke(data.joke)
+    }catch(e){
+      console.error(e)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Jokes</h1>
+      <p>{joke}</p>
+      <button onClick={getData}>Next Joke</button>
     </div>
   );
 }
